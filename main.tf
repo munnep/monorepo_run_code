bb2457d17bc64ffd8dc445b9ba62e20ec5f71613
munnep <patrick.munne@hashicorp.com>
2024-04-09 11:21:09 +0200

    test33

--- main.tf
+++ main.tf
@@ -14,6 +14,3 @@ resource "null_resource" "next" {
 resource "null_resource" "next2" {
   depends_on = [time_sleep.wait_30_seconds]
 }
-resource "null_resource" "next3" {
-  depends_on = [time_sleep.wait_30_seconds]
-}
