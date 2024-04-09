2c15a1e3c26b967e6eed936ac344ab2c080ea75e
munnep <patrick.munne@hashicorp.com>
2024-04-09 11:44:34 +0200

    test

--- test5/main.tf
+++ test5/main.tf
@@ -1,16 +1,16 @@
-resource "null_resource" "previous" {}
+# resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_30_seconds" {
-  depends_on = [null_resource.previous]
+# resource "time_sleep" "wait_30_seconds" {
+#   depends_on = [null_resource.previous]
 
-  create_duration = "60s"
-}
+#   create_duration = "60s"
+# }
 
-# This resource will create (at least) 30 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_30_seconds]
-}
+# # This resource will create (at least) 30 seconds after null_resource.previous
+# resource "null_resource" "next" {
+#   depends_on = [time_sleep.wait_30_seconds]
+# }
 
-resource "null_resource" "next2" {
-  depends_on = [time_sleep.wait_30_seconds]
-}
+# resource "null_resource" "next2" {
+#   depends_on = [time_sleep.wait_30_seconds]
+# }
