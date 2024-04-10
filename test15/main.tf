1cc3dd633f816c31d2ec2011a2db355905943410
munnep <patrick.munne@hashicorp.com>
2024-04-10 11:10:58 +0200

    first run

--- test15/main.tf
+++ test15/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "previous" {}
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [null_resource.previous]
 
-  create_duration = "120s"
+  create_duration = "5s"
 }
 
 # This resource will create (at least) 30 seconds after null_resource.previous
