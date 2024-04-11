f3e5b3e02c690ab81cd1584adb965e6e7ae81b65
munnep <patrick.munne@hashicorp.com>
2024-04-11 14:39:41 +0200

    again a run

--- test62/main.tf
+++ test62/main.tf
@@ -1,22 +1,22 @@
-# resource "random_integer" "priority" {
-#   min = 120
-#   max = 240
-# }
+resource "random_integer" "priority" {
+  min = 120
+  max = 240
+}
 
 
-# resource "null_resource" "previous" {}
+resource "null_resource" "previous" {}
 
-# resource "time_sleep" "wait_x_seconds" {
-#   depends_on = [null_resource.previous]
+resource "time_sleep" "wait_x_seconds" {
+  depends_on = [null_resource.previous]
 
-#   create_duration = "${random_integer.priority.result}s"
-# }
+  create_duration = "${random_integer.priority.result}s"
+}
 
-# # This resource will create (at least) 30 seconds after null_resource.previous
-# resource "null_resource" "next" {
-#   depends_on = [time_sleep.wait_x_seconds]
-# }
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_x_seconds]
+}
 
-# resource "null_resource" "next2" {
-#   depends_on = [time_sleep.wait_x_seconds]
-# }
+resource "null_resource" "next2" {
+  depends_on = [time_sleep.wait_x_seconds]
+}
